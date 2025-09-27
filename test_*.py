3b8b16baecd9c94cf5b83ae0e7b80929e7a9e52e
Trần Minh Thuận <nak.tranthuan@itexam.online>
2025-09-27 10:38:17 +0700

    Delete test_*.py

--- test_*.py
+++
@@ -1,5 +0,0 @@
-import os, cv2
-
-os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp|stimeout;10000000|max_delay;500000"
-cap = cv2.VideoCapture("rtsp://admin:Cmnd024125@192.168.1.10:554/onvif1", cv2.CAP_FFMPEG)
-print("opened:", cap.isOpened())
